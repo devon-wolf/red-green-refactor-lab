55b9197aff17c23f5aa41ac0eab755defc24228d
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-22 15:21:45 -0700

    Destructure getName parameter

--- get-name.js
+++ get-name.js
@@ -1,8 +1,4 @@
-const getName = (someObject) => {
-  const { name } = someObject;
-  if(name) return name;
-};
+const getName = ({ name }) => name;
 
-// note: lab instructions ask for a final version where we 'destructure function parameters' and return name, but I'm not sure how that's different than this?
 
 module.exports = getName;
