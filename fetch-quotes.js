1de4a7e7904e8fd683b36e3d1a1fab12f5b35f2a
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-22 15:24:47 -0700

    Updated Futurama API fetch to use a random-quote-generating route

--- fetch-quotes.js
+++ fetch-quotes.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
 const fetchQuotes = async() => {
-  const res = await fetch('http://futuramaapi.herokuapp.com/api/quotes');
+  const res = await fetch('http://futuramaapi.herokuapp.com/api/quotes/1');
   const body = await res.json();
   
   const { character, quote, image } = body[0];
